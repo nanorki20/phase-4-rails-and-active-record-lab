4936b4f3675e0c26ba8071f6261934acf0021772
Hayet Kumasseh <naaakleh@gmail.com>
2022-07-19 16:05:56 -0400

    commit tree 34c7b4b06c2d1f9f1b59702bc9b384229831a4ab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'clearing out old data..'
+Student.destroy_all
+
+puts 'generating new data..'
+Student.create!(first_name: 'Hayet', last_name: 'Kumasseh', grade: 80)
+Student.create!(first_name: 'Alan', last_name: 'Matos', grade: 90)
+Student.create!(first_name: 'Sabiha', last_name: 'Bary', grade: 100)
+ puts 'done!'
